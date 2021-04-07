65a4511769b9bebab75978afa2bcfabf324e31fb
Chandan <chandangepala@gmail.com>
2021-04-07 12:33:37 +0530

    India Gamer Mall App

---
+++ app/src/main/java/com/basic/indiagamermall/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.basic.indiagamermall.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.basic.indiagamermall.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
