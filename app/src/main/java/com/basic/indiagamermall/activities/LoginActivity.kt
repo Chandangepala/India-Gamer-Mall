cb6baf284ae936bb185479f66ef2cf880457e0d7
Chandan <chandangepala@gmail.com>
2021-05-09 13:23:23 +0530

    India Gamer Mall App Events View Pager

--- app/src/main/java/com/basic/indiagamermall/activities/LoginActivity.kt
+++ app/src/main/java/com/basic/indiagamermall/activities/LoginActivity.kt
@@ -3,12 +3,16 @@ package com.basic.indiagamermall.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.basic.indiagamermall.R
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var  edtEmail: EditText
+    lateinit var edtPassword: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -16,10 +20,21 @@ class LoginActivity : AppCompatActivity() {
         val imgSignIn = findViewById<ImageView>(R.id.imgSignIn)
         val txtSignUp = findViewById<TextView>(R.id.txtSignUP)
 
+        //To initialize all the views...
+        initMain()
+
         imgSignIn.setOnClickListener{
-            val iMainActivity = Intent(this, MainActivity :: class.java)
-            finish()
-            startActivity(iMainActivity)
+
+            if(edtEmail.text != null && edtPassword.text != null){
+                if(edtEmail.text.toString().equals("admin") && edtPassword.text.toString().equals("admin123")){
+                    val iMainActivity = Intent(this, MainActivity :: class.java)
+                    finish()
+                    startActivity(iMainActivity)
+                }else{
+                    Toast.makeText(applicationContext, "Wrong login details, Try again!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
         }
 
         txtSignUp.setOnClickListener{
@@ -27,5 +42,11 @@ class LoginActivity : AppCompatActivity() {
             finish()
             startActivity(iLoginActivity)
         }
+    }
+
+    //To initialize all the views in the layout
+    fun initMain(){
+         edtEmail = findViewById<EditText>(R.id.edtEmail)
+         edtPassword = findViewById<EditText>(R.id.edtPassword)
     }
 }
--- app/src/main/java/com/basic/indiagamermall/activities/MainActivity.kt
+++ app/src/main/java/com/basic/indiagamermall/activities/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.basic.indiagamermall.R
+import com.basic.indiagamermall.adapter.EventViewPagerAdapter
 import com.basic.indiagamermall.adapter.StaggeredRecyclerGameInfoAdapter
 import com.basic.indiagamermall.models.GameInfo
+import pl.pzienowicz.autoscrollviewpager.AutoScrollViewPager
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //To setup and manage auto scroll view pager...
+        setupEventViewPager()
 
         //To fetch items to the arrGameInfoList
         fetchItems()
@@ -48,5 +53,24 @@ class MainActivity : AppCompatActivity() {
             StaggeredGridLayoutManager.VERTICAL)
 
         recyclerGameInfo.adapter = StaggeredRecyclerGameInfoAdapter(this,arrGameInfoList)
+    }
+
+    //funtion to manage event viewpager
+    fun setupEventViewPager(){
+        val eventViewPager: AutoScrollViewPager = findViewById(R.id.view_pager_events)
+        var arrEvents: ArrayList<Int> = ArrayList()
+        addEvents(arrEvents)
+        var eventsAdapter: EventViewPagerAdapter = EventViewPagerAdapter(arrEvents, this)
+        eventViewPager.adapter = eventsAdapter
+        eventViewPager.startAutoScroll(1000)
+    }
+
+    //To add event to arrEvents Arraylist
+    fun addEvents(arrEvents: ArrayList<Int>){
+        arrEvents.add(R.drawable.gamer_logo)
+        arrEvents.add(R.drawable.sumos)
+        arrEvents.add(R.drawable.traffic_tour)
+        arrEvents.add(R.drawable.unicorn_runner)
+        arrEvents.add(R.drawable.warzone)
     }
 }
