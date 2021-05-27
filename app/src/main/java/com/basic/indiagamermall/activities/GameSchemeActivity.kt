e04d3d4b4509d6cb35cdf5333f72abd511a1d82e
Anjali Dadhich <anjalidadhich2101@gmail.com>
2021-05-28 00:52:52 +0530

    Create Whatsapp event on click Buy, some changes in UI

--- app/src/main/java/com/basic/indiagamermall/activities/GameSchemeActivity.kt
+++ app/src/main/java/com/basic/indiagamermall/activities/GameSchemeActivity.kt
@@ -1,7 +1,9 @@
 package com.basic.indiagamermall.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.basic.indiagamermall.R
@@ -33,5 +35,27 @@ class GameSchemeActivity : AppCompatActivity() {
         recyclerGameScheme.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
 
         recyclerGameScheme.adapter = RecyclerGameSchemeAdapter(arrGameSchemeList, this)
+    }
+
+    fun sendMessageViaWhatsapp(message: String, phoneNumber: String) {
+        // send message to a number via whatsapp
+        try {
+            val sendIntent = Intent("android.intent.action.MAIN")
+            sendIntent.putExtra("jid", phoneNumber.toString() + "@s.whatsapp.net")
+            sendIntent.putExtra(Intent.EXTRA_TEXT, message)
+            sendIntent.action = Intent.ACTION_SEND
+            sendIntent.setPackage("com.whatsapp")
+            sendIntent.type = "text/plain"
+            if (intent.resolveActivity(packageManager) == null) {
+                Toast.makeText(this,
+                    "Please install whatsapp first.",
+                    Toast.LENGTH_SHORT).show()
+                return
+            }
+            // Starting Whatsapp
+            startActivity(sendIntent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
--- app/src/main/java/com/basic/indiagamermall/adapter/RecyclerGameSchemeAdapter.kt
+++ app/src/main/java/com/basic/indiagamermall/adapter/RecyclerGameSchemeAdapter.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.basic.indiagamermall.R
+import com.basic.indiagamermall.activities.GameSchemeActivity
 import com.basic.indiagamermall.models.ProductModel
 
 class RecyclerGameSchemeAdapter(private val arrGameSchemeList: ArrayList<ProductModel>, val context: Context) :
@@ -22,10 +24,30 @@ class RecyclerGameSchemeAdapter(private val arrGameSchemeList: ArrayList<Product
     }
 
     override fun onBindViewHolder(holder: RecyclerGameSchemeAdapter.GameSchemeViewHolder, position: Int) {
+        holder.txtTitle.setText("Need Id and password")
+        holder.txtDiamond.setText("1090" + " Diamond")
+        holder.txtPrice.setText("Rs. " + "500")
+        holder.txtBuy.setOnClickListener{
+            val tagLine = "Hiiii, I Want to buy this Game scheme: "
+            val title = "Title: " + holder.txtTitle.text
+            val diamond = "Diamond: " + holder.txtDiamond.text
+            val price = "Price: " + holder.txtPrice.text
 
+            val message = "$tagLine\r \n$title\r \n$diamond\r \n$price"
+            val phoneNumber = "917357678251"
+            // Calling the function
+            if (context is GameSchemeActivity) {
+                (context as GameSchemeActivity). sendMessageViaWhatsapp(message,phoneNumber)
+            }
+
+        }
     }
 
     class GameSchemeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val txtBuy = itemView.findViewById<TextView>(R.id.txtBuy)
+        val txtTitle = itemView.findViewById<TextView>(R.id.txtTitle)
+        val txtDiamond = itemView.findViewById<TextView>(R.id.txtDiamond)
+        val txtPrice = itemView.findViewById<TextView>(R.id.txtPrice)
     }
+
 }
